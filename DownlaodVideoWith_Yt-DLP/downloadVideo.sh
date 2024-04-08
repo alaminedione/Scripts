3186e9439bb12773df5b206ba57e7e55409cf903
alaminedione <alaminedione@skiff.com>
2024-04-08 02:01:52 +0000

    update downloadVideo.sh

--- DownlaodVideoWith_Yt-DLP/downloadVideo.sh
+++ DownlaodVideoWith_Yt-DLP/downloadVideo.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+# activer l'utilisation des scripts 
+shopt -s expand_aliases
+
 # Définition des variables
 LOG_FILE="download.log"
 VIDEO_FILE="listvideo.txt"
 ECHEC_FILE="echoue.txt"
 TERMINE_FILE="termine.txt"
 DOSSIER_VIDEOS="videos_telechargees"
+
+#[yt-dlp]
+# Options pour la meilleure qualité vidéo MP4
+yt_dlp_qualite="-f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]'"
+yt_dlp_subs="--write-sub --sub-lang en,fr"
+yt_dlp_auto_subs="--write-auto-sub --sub-lang en,fr"
+yt_dlp_downloader="--downloader aria2c --downloader-args '-c -j 3 -x 3 -s 3 -k 1M'"
+yt_dlp_audio="-x -f 'bestaudio[ext=m4a]'"
+
+alias yt_dlp_q="yt-dlp ${yt_dlp_qualite} ${yt_dlp_downloader}"
+alias yt_dlp_q_s="yt-dlp ${yt_dlp_qualite} ${yt_dlp_downloader} ${yt_dlp_subs}"
+alias yt_dlp_q_as="yt-dlp ${yt_dlp_qualite} ${yt_dlp_downloader} ${yt_dlp_auto_subs}"
+alias yt_dlp_x="yt-dlp ${yt_dlp_downloader} ${yt_dlp_audio}"
+alias yt_dlp_test="yt-dlp ${yt_dlp_qualite}"
 
 # Vérification de l'existence du fichier listvideo.txt
 if [ ! -f "$VIDEO_FILE" ]; then
@@ -22,7 +39,14 @@ log() {
 touch "$ECHEC_FILE"
 touch "$TERMINE_FILE"
 
-log "Début du téléchargement des vidéos."
+ echo -e " \n
+            --------------------------------------------------------------
+             -- scripte execute à :  -- $(date)' -- 
+            --------------------------------------------------------------
+         "  >> "$LOG_FILE"
+
+
+log "  ----- Début du téléchargement des vidéos.  ----- \n "
 
 # Création du dossier pour les vidéos téléchargées
 mkdir -p "$DOSSIER_VIDEOS"
@@ -37,7 +61,7 @@ while IFS= read -r video_link || [ -n "$video_link" ]; do
 
     echo "Vidéo en cours de téléchargement"
     # Téléchargement de la vidéo avec yt-dlp et déplacement dans le dossier videos_telechargees
-    yt-dlp -o "$DOSSIER_VIDEOS/%(title)s.%(ext)s" "$video_link"
+    yt_dlp_q -o "$DOSSIER_VIDEOS/%(title)s.%(ext)s" "$video_link"
 
     if [ $? -eq 0 ]; then
         log "Téléchargement réussi pour la vidéo : $video_link \n"
@@ -51,10 +75,11 @@ while IFS= read -r video_link || [ -n "$video_link" ]; do
 
 done < "$VIDEO_FILE"
 
-log "Execution du script terminé"
+log "Execution du script terminé   ---------------------------------------------------------------------------------------------"
 echo "Execution du script terminé"
 
-# supprimer la liste de liens des videos sur la fichier listvideo.txt
-echo "" > "$VIDEO_FILE"
+cp $ECHEC_FILE  $VIDEO_FILE  # option -n = no-clobber i.e. without overiting
+
+echo "" > "$ECHEC_FILE"
 
 echo "Veuillez consulter le fichier $LOG_FILE pour plus de détails."
